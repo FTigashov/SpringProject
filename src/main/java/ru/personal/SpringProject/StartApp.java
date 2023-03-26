d0e6d4859759721d502f3793ac337c1d71fe50fe
FTigashov <tigashov.filipp@yandex.ru>
2023-03-26 23:11:18 +0300

    created static html pages

--- src/main/java/ru/personal/SpringProject/StartApp.java
+++ src/main/java/ru/personal/SpringProject/StartApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProjectApplication {
+public class StartApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringProjectApplication.class, args);
+		SpringApplication.run(StartApp.class, args);
 	}
 }
